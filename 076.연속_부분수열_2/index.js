555052d01783ef1fbac6db51d8ade99bf482fda6
uhjee <oper1490@gmail.com>
2021-10-27 20:52:20 +0900

    '[coding-test]076'

---
+++ 076.연속_부분수열_2/index.js
@@ -0,0 +1,16 @@
+function solution(m, arr) {
+  const len = arr.length;
+  let count = 0;
+  let p1 = 0;
+  
+  while(p1 < len) {
+    let p2 = p1;
+    let sum = 0;
+    while(p2 < len && sum < m)
+  }
+
+
+}
+
+let a = [1, 3, 1, 2, 3];
+console.log(solution(5, a));
