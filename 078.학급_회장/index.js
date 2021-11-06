15efcfa3089005ad5001f399bbfbe1075e92d437
uhjee <oper1490@gmail.com>
2021-11-06 23:33:24 +0900

    [coding-test] 078.학급 회장
    - hash

--- 078.학급_회장/index.js
+++ 078.학급_회장/index.js
@@ -13,5 +13,25 @@ function solution(s) {
   )[0];
 }
 
+// ! 선생님 코드 Map 사용
+function solution1(s) {
+  let answer;
+  let m = new Map();
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let i of s) {
+    if (m.has(i)) m.set(i, m.get(i) + 1);
+    else m.set(i, 0);
+  }
+  console.log(m);
+  for (let [key, val] of m) {
+    if (val > max) {
+      max = val;
+      answer = key;
+    }
+  }
+  return answer;
+}
+
 let str = 'BACBACCACCBDEDE';
-console.log(solution(str));
+console.log(solution1(str));
