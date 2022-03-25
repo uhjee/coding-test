d9def0e8452b4f14b224b7d3e43560670ee0729e
uhjee <oper1490@gmail.com>
2022-03-25 19:49:14 +0900

    091.삽입 정렬

--- 091.삽입_정렬/index.js
+++ 091.삽입_정렬/index.js
@@ -2,6 +2,7 @@ function solution(arr) {
   let answer = [...arr];
   for (let i = 1; i < answer.length; i++) {
     let nextIndex = i;
+    // j는 i-1 부터 0까지 전부 돈다.
     for (let j = i - 1; j > -1; j--) {
       if (answer[j] > answer[i]) {
         nextIndex = j;
@@ -14,5 +15,19 @@ function solution(arr) {
   return answer;
 }
 
+const solution1 = arr => {
+  let answer = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = answer[i];
+    let j = null;
+    for (j = i - 1; j >= 0; j--) {
+      if (answer[j] > tmp) answer[j + 1] = answer[j];
+      else break;
+    }
+    answer[j + 1] = tmp;
+  }
+  return answer;
+};
+
 let arr = [11, 7, 5, 6, 10, 9];
-console.log(solution(arr));
+console.log(solution1(arr));
