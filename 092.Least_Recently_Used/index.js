b8ecc40c5b09df21ca97ea701bc75467132f9893
uhjee <oper1490@gmail.com>
2022-03-28 21:38:48 +0900

    092.Least Recently Used

--- 092.Least_Recently_Used/index.js
+++ 092.Least_Recently_Used/index.js
@@ -1,33 +1,13 @@
-function solution(arr) {
-  let answer = [...arr];
-  for (let i = 1; i < answer.length; i++) {
-    let nextIndex = i;
-    // j는 i-1 부터 0까지 전부 돈다.
-    for (let j = i - 1; j > -1; j--) {
-      if (answer[j] > answer[i]) {
-        nextIndex = j;
-      }
-    }
-    const [iValue] = answer.splice(i, 1);
-    answer.splice(nextIndex, 0, iValue);
-    console.log(answer);
-  }
+function solution(size, arr) {
+  let answer = [];
+  arr.forEach(i => {
+    const cIndex = answer.findIndex(c => i === c);
+    if (cIndex > -1) answer.splice(cIndex, 1);
+    answer.unshift(i);
+    if (answer.length > size) answer.pop();
+  });
   return answer;
 }
 
-const solution1 = arr => {
-  let answer = [...arr];
-  for (let i = 0; i < arr.length; i++) {
-    let tmp = answer[i];
-    let j = null;
-    for (j = i - 1; j >= 0; j--) {
-      if (answer[j] > tmp) answer[j + 1] = answer[j];
-      else break;
-    }
-    answer[j + 1] = tmp;
-  }
-  return answer;
-};
-
-let arr = [11, 7, 5, 6, 10, 9];
-console.log(solution1(arr));
+let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
