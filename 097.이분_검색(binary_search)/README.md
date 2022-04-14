f80c6d7c88a9b699af7f6c3708b58cc6dacdeb48
uhjee <oper1490@gmail.com>
2022-04-14 22:21:46 +0900

    097.이분 검색 (binary searching)

--- 097.이분_검색(binary_search)/README.md
+++ 097.이분_검색(binary_search)/README.md
@@ -25,5 +25,67 @@
 ## 풀이
 
 ```js
+function solution(target, arr) {
+  let answer = 0;
+  let copiedArr = [...arr];
 
+  copiedArr.sort((a, b) => a - b);
+  let i = 0; // 혹시 모를 infinite loop을 위해 제한
+
+  // searching boundary
+  let lt = 0;
+  let rt = copiedArr.length - 1;
+
+  while (i < 30) {
+    midIndex = Math.round((lt + rt) / 2);
+
+    if (target === copiedArr[midIndex]) {
+      answer = midIndex;
+      break;
+    } else if (target < copiedArr[midIndex]) {
+      rt = midIndex - 1;
+    } else {
+      lt = midIndex;
+    }
+    console.log({ midIndex });
+
+    i++;
+  }
+
+  return answer + 1;
+}
 ```
+
+### 선생님 풀이
+
+```js
+const solution1 = (target, arr) => {
+  let answer;
+  arr.sort((a, b) => a - b); // 이분 검색은 선 정렬 필수
+
+  let lt = 0;
+  let rt = arr.length - 1;
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) rt = mid - 1;
+    else lt = mid + 1;
+  }
+  return answer;
+};
+
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution1(32, arr));
+```
+
+- 이분 검색
+  - 시간 복잡도: O(logN)
+
+- 순차 검색
+  - 시간 복잡도: O(N)
+
+- lt, rt 를 포인터로 두어, 검색 범위를 좁힌다.
+- while문의 조건으로 lt 가 rt보다 작거나 같을 동안만 loop
--- 097.이분_검색(binary_search)/index.js
+++ 097.이분_검색(binary_search)/index.js
@@ -3,26 +3,48 @@ function solution(target, arr) {
   let copiedArr = [...arr];
 
   copiedArr.sort((a, b) => a - b);
-  const getMidNum = (start, end) => Math.round((end - start) / 2);
-  let item = target;
-  let midIndex = getMidNum(0, copiedArr.length - 1);
-  let i = 0;
+  let i = 0; // 혹시 모를 infinite loop을 위해 제한
+
+  // searching boundary
+  let lt = 0;
+  let rt = copiedArr.length - 1;
+
   while (i < 30) {
-    if (item === copiedArr[midIndex]) {
+    midIndex = Math.round((lt + rt) / 2);
+
+    if (target === copiedArr[midIndex]) {
       answer = midIndex;
       break;
-    } else if (item < copiedArr[midIndex]) {
-      copiedArr = copiedArr.slice(midIndex - (copiedArr.length - 1));
+    } else if (target < copiedArr[midIndex]) {
+      rt = midIndex - 1;
     } else {
-      copiedArr = copiedArr.slice(midIndex);
+      lt = midIndex;
     }
-    midIndex = getMidNum(0, copiedArr.length - 1);
+    console.log({ midIndex });
 
     i++;
   }
 
-  return answer;
+  return answer + 1;
 }
 
+const solution1 = (target, arr) => {
+  let answer;
+  arr.sort((a, b) => a - b); // 이분 검색은 선 정렬 필수
+
+  let lt = 0;
+  let rt = arr.length - 1;
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) rt = mid - 1;
+    else lt = mid + 1;
+  }
+  return answer;
+};
+
 let arr = [23, 87, 65, 12, 57, 32, 99, 81];
-console.log(solution(32, arr));
+console.log(solution1(32, arr));
