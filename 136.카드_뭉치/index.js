f4a8fe1969115f3770bde96d63ce12cd674a1b28
uhjee <oper1490@gmail.com>
2026-06-25 00:14:11 +0900

    136.코드뭉치_queue

--- 136.카드_뭉치/index.js
+++ 136.카드_뭉치/index.js
@@ -1,4 +1,5 @@
-const solution = (cards1, cards2, goal) => {
+// 01. Arrays.shift() 사용: 시간 복잡도:O(N²)
+const solution1 = (cards1, cards2, goal) => {
   let result = 'YES';
 
   let cards1Cp = [...cards1];
@@ -18,6 +19,58 @@ const solution = (cards1, cards2, goal) => {
   return result;
 };
 
+// 02. Queue 구현 - 시간복잡도 O(N)
+
+class Queue {
+  items = [];
+  front = 0;
+  rear = 0;
+
+  constructor(arr) {
+    this.items = arr;
+    this.rear = arr.length;
+  }
+
+  push(item) {
+    this.items.push(item);
+    this.rear++;
+  }
+
+  pop() {
+    return this.items[this.front++];
+  }
+
+  first() {
+    return this.items[this.front];
+  }
+
+  isEmpty() {
+    return this.front === this.rear;
+  }
+}
+
+const solution = (cards1, cards2, goal) => {
+  let result = 'Yes';
+  const cards1Q = new Queue(cards1);
+  const cards2Q = new Queue(cards2);
+  const goalQ = new Queue(goal);
+
+  while (!goalQ.isEmpty()) {
+    if (goalQ.first() === cards1Q.first()) {
+      cards1Q.pop();
+    } else if (goalQ.first() === cards2Q.first()) {
+      cards2Q.pop();
+    } else {
+      result = 'No';
+      break;
+    }
+    goalQ.pop();
+  }
+
+  return result;
+};
+
+// Test
 const param1 = [
   ['i', 'drink', 'water'],
   ['want', 'to'],
@@ -29,7 +82,6 @@ const param2 = [
   ['want', 'to'],
   ['i', 'want', 'to', 'drink', 'water'],
 ];
-
 
 console.log(solution(param1[0], param1[1], param1[2]));
 console.log(solution(param2[0], param2[1], param2[2]));
